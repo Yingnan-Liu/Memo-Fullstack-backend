7e98b76f0219ed6e0034fa2bae7dfa31f59a5cba
Lynn <yingnanliu.lynn@gmail.com>
2021-07-11 23:15:58 +0800

    mongoose校验

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message);
   if (error.name === "CastError" && error.kind === "ObjectId") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
