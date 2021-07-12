c1b54c342fe928e093a3d1ffb759f094de255aa0
Lynn <yingnanliu.lynn@gmail.com>
2021-07-12 11:25:45 +0800

    user management

--- index.js
+++ index.js
@@ -1,101 +1,10 @@
-//nodejs使用commonjs 但是迟早会支持ES6de
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const Note = require("./models/note");
-const cors = require("cors");
+const app = require("./app");
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-app.use(express.static("build"));
-app.use(cors());
-app.use(express.json());
+const server = http.createServer(app);
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  if (error.name === "CastError" && error.kind === "ObjectId") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-  next(error);
-};
-// const generateId = () => {
-//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-//   return maxId + 1;
-// };
-
-// 处理/发出的get请求
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World! !</h1>");
-  console.log("here here");
-  // console.log(PORT);
-  // console.log(process.env);
-});
-
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (request, response, next) => {
-  //mongoose的findById方法
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => {
-      console.log("error:");
-      next(error);
-    });
-});
-
-// 删除资源  findByIdAndRemove mongoose方法
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-//put 更换重要性
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-//post请求
-app.post("/api/notes", (request, response) => {
-  const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({
-      err: "content missing",
-    });
-  }
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
-});
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
--- models/note.js
+++ models/note.js
@@ -1,34 +1,20 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
   date: {
     type: Date,
     required: true,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 noteSchema.set("toJSON", {
